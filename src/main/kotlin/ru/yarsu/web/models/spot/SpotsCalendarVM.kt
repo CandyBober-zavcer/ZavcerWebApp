41593af7f4a37bc1843434f487bfc4f6374c47c6
Petr <pakazakovyar@yandex.ru>
2025-06-13 21:04:07 +0300

    Revert "кошмар"
    This reverts commit d2637d1e145b0697d9d1e2127e09d5bd07af2f46.

--- src/main/kotlin/ru/yarsu/web/models/spot/SpotsCalendarVM.kt
+++
@@ -1,6 +0,0 @@
-package ru.yarsu.web.models.spot
-
-import org.http4k.template.ViewModel
-
-class SpotsCalendarVM: ViewModel {
-}
--- src/main/kotlin/ru/yarsu/web/models/teacher/TeacherCalendar2VM.kt
+++
@@ -1,7 +0,0 @@
-package ru.yarsu.web.models.teacher
-
-import org.http4k.core.HttpHandler
-import org.http4k.template.ViewModel
-
-class TeacherCalendar2VM: ViewModel {
-}
